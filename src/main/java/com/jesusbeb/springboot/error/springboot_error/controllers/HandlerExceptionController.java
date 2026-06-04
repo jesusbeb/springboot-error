9ac80288c8d007ee637fa53dde0b33e41e58be1a
jesusbeb <jesusbelb@gmail.com>
2026-06-04 12:43:36 -0600

    Añadiendo metodo handler para el manejo de excepcion

--- src/main/java/com/jesusbeb/springboot/error/springboot_error/controllers/HandlerExceptionController.java
+++ src/main/java/com/jesusbeb/springboot/error/springboot_error/controllers/HandlerExceptionController.java
@@ -1,21 +1,31 @@
 package com.jesusbeb.springboot.error.springboot_error.controllers;
 
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.jesusbeb.springboot.error.springboot_error.models.Error;
 
 // @ControllerAdvice es un controlador que no esta mapeado a una ruta, sino que se encarga de manejar las excepciones que ocurren en los controladores de la aplicación.
 @ControllerAdvice
 public class HandlerExceptionController {
 
     // @ExceptionHandler es una anotación que se le indica el tipo de excepción que se va a manejar
-    // El metodo devuelve un ResponseEntity que va en el cuerpo el mensaje de error. Se usa ? para indicar que el tipo de dato es generico, ya que puede ser un String, un objeto, etc.
-    // Exception ex es el objeto que contiene la información de la excepción que se ha lanzado.
-    // internalServerError() es un metodo de ResponseEntity que indica que el error es un error interno del servidor (500). body() es un metodo que se usa para indicar el cuerpo de la respuesta, en este caso el mensaje de error.
+    // El metodo devuelve un ResponseEntity de tipo Error, que es un objeto que contiene la información del error que se ha producido.
+    // Exception ex es el parametro que recibe el metodo, el cual es un objeto de tipo Exception que contiene la información de la excepción que se ha lanzado.
+    // Creamos una instancia de la clase Error, que es la que personaliza la respuesta de error que se va a enviar al cliente.
     @ExceptionHandler(ArithmeticException.class)
-    public ResponseEntity<?> divisionByZero(Exception ex) {
+    public ResponseEntity<Error> divisionByZero(Exception ex) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Division por cero!!!");
+        error.setMessage(ex.getMessage());
+        error.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
 
-        return ResponseEntity.internalServerError().body("error 500!!!");
+        return ResponseEntity.internalServerError().body(error);
     }
 
 }
